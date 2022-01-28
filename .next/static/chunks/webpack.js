13521f9aee55a965ec7ce0dd9ee311758e292c51
Caio Magalhaes <caiohenriquemagalhaes@hotmail.com>
2022-01-28 16:04:02 -0300

    Estilo em todas as paginas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "514e4a9e872bae9c"; }
+/******/ 		__webpack_require__.h = function() { return "fe6d7cd2bea11949"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
